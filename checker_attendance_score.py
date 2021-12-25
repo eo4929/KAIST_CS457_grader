c43f3d9ce9a8f28a9019b7d42a5ba985fc76cffc
Dae-Young Park <61178057+eo4929@users.noreply.github.com>
2021-12-25 16:06:13 +0900

    used for CS459 service computing

---
+++ checker_attendance_score.py
@@ -0,0 +1,118 @@
+students = []
+input_filename = './attend/participants_cs459_lec8_20210930.csv'
+outout_filename = 'out_attendance_0930.csv'
+
+with open('students_cs459.csv', 'r', encoding='utf-8') as f: # 학생 리스트
+    f.readline()
+
+    while True:
+        line = f.readline()
+        if not line: break
+
+        items = line.split(',')
+        students.append({
+            'id': items[2],
+            'name': items[3],
+            'attendance': 'None'
+        })
+
+students = students[0:33]
+
+#print(students)
+
+for i in range(len(students)): # checking same rows (students)
+    for j in range(i+1, len(students)):
+        if students[i]['id'] == students[j]['id']:
+            print('!!!!!!!!!!!!!!!!!!!!!!!!!', i, j)
+        if students[i]['name'] == students[j]['name']:
+            print('!!!!!!!!!!!!!!!!!!!!!!!!!', i, j)
+
+
+with open(input_filename, 'r', encoding='utf-8') as f: # 교수님이 주시는 duration 파일
+    lines = f.readlines()
+
+#print(lines)
+#print()
+
+items = lines[1].rstrip('\n').split(',')
+
+duration = int( items[5] )
+print('duration of professor = ', duration)
+
+lines = lines[8:]
+
+"""print()
+print(lines)
+print()"""
+
+not_late = 9999
+absent = 9999
+
+not_late = duration * 0.8 - 5
+absent = duration * 0.5 - 5
+print('[지각 기준:', not_late, '결석 기준:', absent, ']\n')
+
+for i in range(0, len(lines)):
+    if lines[i] != '\n' and lines[i] != ',,\n':
+        items = lines[i].rstrip('\n').split(',')
+        name = items[0].strip()
+        email = items[1].strip()
+        duration = int('0' if items[2] == '' else items[2])
+        attendance = 'be present'
+
+        #if name == 'In-Young Ko':
+        #not_late = duration * 0.8 + 2 # adding 2 minutes
+        #absent = duration * 0.5 + 2 # adding 2 minutes
+
+
+
+
+num_of_participants = 0
+for i in range(0, len(lines)): # 학생당 출석/지각/결석 여부 -> attendance 변수에 저장
+    if lines[i] != '\n':
+        items = lines[i].rstrip('\n').split(',')
+
+        num_of_participants +=1
+        print('items= ', end='')
+        print(items)
+        name = items[0].strip()
+        email = items[1].strip()
+        duration = int('0' if items[2] == '' else items[2])
+        #print('duration = ', duration)
+        attendance = 'be absent'
+
+        if duration >= not_late:
+            attendance = 'be present'
+        elif duration >= absent:
+            attendance = 'be late'
+
+        #print('attendance = ', attendance)
+
+        check = False
+        for j in range(len(students)):
+            if students[j]['name'].strip() in name: # 아 뭐라도 줌 name에 들어가면 student 속 사람이랑 줌에 접속한 사람이랑 동일한 걸로 취급했구나
+                students[j]['attendance'] = attendance
+                check = True
+            elif students[j]['id'].strip() in name:
+                students[j]['attendance'] = attendance
+                check = True
+
+            if check == True:
+                break
+        if not check: # 이쪽으로 가는 케이스 때문에 Zoom 유저 네임에 대한 정책이 필요한 것임 -> 아마 정연이는 수작업으로 한듯..?
+            print('출석/지각/결석 여부: ',end='')
+            print(attendance)
+
+print()
+print('참가자 수: ', num_of_participants)
+print()
+
+with open(outout_filename, 'w', encoding="utf-8") as f:
+
+    for student in students:
+        #f.write(student['id'] + ',' + student['name'] + ',' + student['attendance'] + '\n')
+        f.write(student['id'] + ',' + student['attendance'] + '\n')
+
+
+
+
---
+++ checker_participation_score.py
@@ -0,0 +1,80 @@
+students = []
+input_filename = './chat/chat_cs459_finalpresentation_20211216.txt'
+outout_filename = 'out_participation_1216.csv'
+
+with open('students_cs459.csv', 'r', encoding='utf-8') as f:
+    f.readline()
+
+    while True:
+        line = f.readline()
+        if not line: break
+
+        items = line.split(',')
+        students.append({
+            'id': items[2],
+            'name': items[3],
+            'participation': 0
+        })
+
+students = students[0:33]
+
+print(students)
+
+for i in range(len(students)): # checking same rows (students)
+    for j in range(i+1, len(students)):
+        if students[i]['id'] == students[j]['id']:
+            print('!!!!!!!!!!!!!!!!!!!!!!!!!', i, j)
+        if students[i]['name'] == students[j]['name']:
+            print('!!!!!!!!!!!!!!!!!!!!!!!!!', i, j)
+        
+
+with open(input_filename, 'r', encoding='UTF8') as f:
+    lines = f.readlines()
+
+print('len(line): ', len(lines))
+print('lines: ', lines)
+
+new_lines = [] # Thank you, Yes, No , Ok 포함한 원소는 제거한 lines
+
+found = [-1,-1,-1,-1,-1,-1,-1,-1]
+for line in lines:
+    found[0] = line.find('Thank you') # 인자 포함한 인덱스 반환
+    found[1] = line.find('thank you')
+    found[2] = line.find('Yes')
+    found[3] = line.find('yes')
+    found[4] = line.find('No')
+    found[5] = line.find('no')
+    found[6] = line.find('Ok')
+    found[7] = line.find('ok')
+
+    if sum(found) == -8:
+        new_lines.append(line)
+    found = [-1,-1,-1,-1,-1,-1,-1,-1]
+
+print('len(new_lines): ', len(new_lines))
+print('new_lines: ', new_lines)
+
+for i in range(len(new_lines)):
+    if new_lines[i] != '\n':
+        # items = lines[i].split()
+        # items = set(items)
+
+        check = False
+        for j in range(len(students)):
+            if students[j]['name'].strip() in new_lines[i]:
+                students[j]['participation'] += 1
+                check = True
+            elif students[j]['id'].strip() in new_lines[i]:
+                students[j]['participation'] += 1
+                check = True
+            
+            if check == True:
+                break
+        if not check: # zoom 유저 네임에 id나 실제 name(student_cs457.csv 상) 관련된 게 없으면 일로 간다->수작업으로 참가점수 카운팅하기 위한 코드
+            print(new_lines[i])
+
+
+with open(outout_filename, 'w') as f:
+    for student in students:
+        #f.write(student['id'] + ',' + student['name'] + ',' + str(student['participation']) + '\n')
+        f.write(student['id'] + ',' + str(student['participation']) + '\n')
